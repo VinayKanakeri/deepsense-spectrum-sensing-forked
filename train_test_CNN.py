1555ae73feb41979ba4bbc7c2017786dcbe05422
Vinay Kanakeri <vinaykanakeri4580@gmail.com>
2022-11-16 18:51:57 -0800

    WIP. Modifications to train LTS dta

--- train_test_CNN.py
+++ train_test_CNN.py
@@ -22,68 +22,75 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, LeakyReLU, Flatten, Input
 from tensorflow.keras.models import Model
 import argparse
+import os
 
 # Parse Arguments
 parser = argparse.ArgumentParser(description='Training or testing CNN')
 parser.add_argument('mode', type=str, default="train", help="choose training/testing mode")
 args = parser.parse_args()
 
+#Model parameters
+n_classes = 16       #number of classes for SDR case
+dim = 32    #Number of I/Q samples being taken as input
+n_channels = 2      #One channel for I and the other for Q
+
+#Build model
+inputs = Input(shape=(dim, n_channels))
+x = Conv1D(16, 3, input_shape=(dim, n_channels), name='conv1')(inputs)
+x = LeakyReLU(alpha=0.1)(x)
+x = Conv1D(16, 3, name='conv2')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling1D(pool_size=2, strides=2, name='pool1')(x)
+x = Conv1D(32, 5, name='conv3')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = Conv1D(32, 5, name='conv4')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling1D(pool_size=2, strides=2, name='pool2')(x)
+x = Flatten()(x)
+x = Dense(64, name='dense1')(x)
+x = LeakyReLU(alpha=0.1)(x)
+outputs = Dense(n_classes, activation='sigmoid', name='out')(x)
+model = Model(inputs=inputs, outputs=outputs)
+model.summary()
+
 #Open dataset .h5 file either for training or testing
-dset_fp = './sdr_wifi_' + args.mode + '.h5'
-dset = h5py.File(dset_fp, 'r')
-X = dset['X'][()]
-y = dset['y'][()]
+dset_fp = [file for file in os.listdir("./lte-dataset/") if args.mode in file] 
+for file in dset_fp:
+    dset = h5py.File("./lte-dataset/" + file, 'r')
+    X = dset['X'][()]
+    y = dset['y'][()]
+
+    X = np.swapaxes(X, 0, 2)
+    y = np.swapaxes(y, 0, 1)
+
+    # print(f'X: {X.shape}')
+    # print(f'Y: {y[:,0]}')
 
 
+    if args.mode == 'train':
 
-if args.mode == 'train':
+        #Compile Model
+        adam = tf.keras.optimizers.Adam(lr=0.001)
+        model.compile(loss='binary_crossentropy',
+                    optimizer=adam,
+                    metrics=['accuracy'])
 
-    #Model parameters
-    n_classes = 4       #number of classes for SDR case
-    dim = X.shape[1]    #Number of I/Q samples being taken as input
-    n_channels = 2      #One channel for I and the other for Q
+        #Train
+        model.fit(x=X, y=y, validation_split=0.1, batch_size=256, epochs=150, verbose=1, shuffle=True)
+        model.save('deepsense-model-trained')
 
-    #Build model
-    inputs = Input(shape=(dim, n_channels))
-    x = Conv1D(16, 3, input_shape=(dim, n_channels), name='conv1')(inputs)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = Conv1D(16, 3, name='conv2')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = MaxPooling1D(pool_size=2, strides=2, name='pool1')(x)
-    x = Conv1D(32, 5, name='conv3')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = Conv1D(32, 5, name='conv4')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    x = MaxPooling1D(pool_size=2, strides=2, name='pool2')(x)
-    x = Flatten()(x)
-    x = Dense(64, name='dense1')(x)
-    x = LeakyReLU(alpha=0.1)(x)
-    outputs = Dense(n_classes, activation='sigmoid', name='out')(x)
-    model = Model(inputs=inputs, outputs=outputs)
-    model.summary()
+    elif args.mode == 'test':
 
-    #Compile Model
-    adam = tf.keras.optimizers.Adam(lr=0.001)
-    model.compile(loss='binary_crossentropy',
-                  optimizer=adam,
-                  metrics=['accuracy'])
+        #Load a pretrained model
+        model = tf.keras.models.load_model(".../filepath_to_trained_model.h5")
 
-    #Train
-    model.fit(x=X, y=y, validation_split=0.1, batch_size=256, epochs=150, verbose=1, shuffle=True)
+        #Compile model
+        adam = tf.keras.optimizers.Adam(lr=0.001)
+        model.compile(loss='binary_crossentropy',
+                    optimizer=adam,
+                    metrics=['accuracy'])
 
-
-elif args.mode == 'test':
-
-    #Load a pretrained model
-    model = tf.keras.models.load_model(".../filepath_to_trained_model.h5")
-
-    #Compile model
-    adam = tf.keras.optimizers.Adam(lr=0.001)
-    model.compile(loss='binary_crossentropy',
-                  optimizer=adam,
-                  metrics=['accuracy'])
-
-    #Test model
-    score = model.evaluate(x=X, y=y, verbose=1)
-    print('Loss: ' + str(score[0]))
-    print('Acc: ' + str(score[1]))
+        #Test model
+        score = model.evaluate(x=X, y=y, verbose=1)
+        print('Loss: ' + str(score[0]))
+        print('Acc: ' + str(score[1]))
